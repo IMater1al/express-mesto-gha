9d9acf423382869c6941ae1de4d3bb6c6ebf3b1f
Karen Petrosian <karenpetrosyan109@yandex.ru>
2024-01-24 19:43:12 +0500

    fix: исправление ошибок автотеста

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ const { getJwtToken } = require('../utils/jwt');
 const ServerError = require('../errors/ServerError');
 const NotFoundError = require('../errors/NotFoundError');
 const UnauthorizedError = require('../errors/UnauthorizedError');
-const BadRequestError = require('../errors/BadRequestError');
+const ConflictError = require('../errors/ConflictError');
 
 const { SALT_ROUNDS = 10 } = process.env;
 
@@ -35,12 +35,7 @@ module.exports.getCurrentUser = (req, res, next) => {
   const currentUserId = req.user._id;
   User.findById(currentUserId)
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(BAD_REQUEST).send({ message: 'Введите валидный _id' });
-      }
-      return next(new ServerError('Произошла ошибка сервера'));
-    });
+    .catch(() => next(new ServerError('Произошла ошибка сервера')));
 };
 
 module.exports.createUser = (req, res, next) => {
@@ -49,15 +44,20 @@ module.exports.createUser = (req, res, next) => {
   } = req.body;
 
   User.findOne({ email }).then((user) => {
-    if (user) return next(new BadRequestError('Такой пользователь уже существует'));
+    if (user) return next(new ConflictError('Такой пользователь уже существует'));
 
     return bcrypt.hash(password, SALT_ROUNDS)
       .then((hash) => User.create({
         name, about, avatar, email, password: hash,
       }))
-      .then((newUser) => res.status(CREATED).send(newUser));
+      .then((newUser) => res.status(CREATED).send({
+        name: newUser.name,
+        about: newUser.about,
+        avatar: newUser.avatar,
+        email: newUser.email,
+      }));
   })
-    .catch(() => next(new ServerError('Произошла ошибка сервера')));
+    .catch((err) => next(new ServerError(err.message)));
 };
 
 module.exports.updateUserInfo = (req, res, next) => {
